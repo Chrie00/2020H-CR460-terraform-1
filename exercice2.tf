786ecbc3d507cf46f1a6e471efdb34d45cb8e4e0
Chrie00 <christine.mathieu72@gmail.com>
2021-02-06 17:42:59 -0500

    modification
    Modification des paramètrer réseau et sous réseau

--- exercice2.tf
+++ exercice2.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = "zonedetest"
+  project = "test-cr460"
   credentials = "account.json"
   region  = "us-central1"
   zone    = "us-central1-c"
@@ -8,6 +8,7 @@ provider "google" {
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
+  tags         = ["workload"]
 
   boot_disk {
     initialize_params {
@@ -16,7 +17,7 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   network_interface {
-    network = google_compute_network.vpc_network.self_link
+    subnetwork = google_compute_subnetwork.network-with-private-secondary-ip-ranges.self_link
     access_config {
     }
   }
@@ -24,5 +25,24 @@ resource "google_compute_instance" "vm_instance" {
 
 resource "google_compute_network" "vpc_network" {
   name                    = "terraform-network"
-  auto_create_subnetworks = "true"
+  auto_create_subnetworks = "false"
+}
+resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
+  name          = "test-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
+  secondary_ip_range {
+    range_name    = "tf-test-secondary-range-update1"
+    ip_cidr_range = "192.168.10.0/24"
+  }
+}
+resource "google_compute_firewall" "ssh-workload" {
+  name    = "ssh-workload"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  target_tags=["workload"]
 }
