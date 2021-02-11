a32408cb9055063db43a7bcd1f81695f3da9f0e8
Chrie00 <christine.mathieu72@gmail.com>
2021-02-11 11:27:00 -0500

    personalisation

--- exercice2.tf
+++ exercice2.tf
@@ -1,4 +1,4 @@
-provider "google" {
+provider "google" {provider "google" {
   project = "test-cr460"
   credentials = "account.json"
   region  = "us-central1"
@@ -27,19 +27,23 @@ resource "google_compute_network" "vpc_network" {
   name                    = "terraform-network"
   auto_create_subnetworks = "false"
 }
+
 resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
   name          = "test-subnetwork"
   ip_cidr_range = "10.2.0.0/16"
   region        = "us-central1"
   network       = google_compute_network.vpc_network.id
+
   secondary_ip_range {
     range_name    = "tf-test-secondary-range-update1"
     ip_cidr_range = "192.168.10.0/24"
   }
 }
+
 resource "google_compute_firewall" "ssh-workload" {
   name    = "ssh-workload"
   network = google_compute_network.vpc_network.name
+
   allow {
     protocol = "tcp"
     ports    = ["22"]
